9f4d65ac7237a9f4e248f1770f82b4c8c893b5b1
SA <no@email.com>
2022-12-23 12:23:57 +0900

    obvious

---
+++ homebox_hosts_update.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/bash
+
+cp /etc/hosts /etc/hosts_old
+rm -f /tmp/tmp_hosts
+awk '!/homebox/ && !/120s' /etc/hosts > /tmp/tmp_hosts && mv /tmp/tmp_hosts /etc/hosts
+
+# the 'raw file' on github is not updated properly.
+#echo `curl -s https://raw.githubusercontent.com/crtoled/ip-public/main/ip.txt | tail -n1` 'homebox'  >> /etc/hosts
+
+git clone https://github.com/crtoled/ip-public.git /tmp/homebox
+#echo `cat /tmp/homebox/ip.txt | tail -n1` 'homebox'  >> /etc/hosts
+echo `cat /tmp/homebox/ip-box2022.txt | tail -n1` '     homebox'  >> /etc/hosts
+echo `cat /tmp/homebox/ip-120s.txt | tail -n1` '     120s'  >> /etc/hosts
+
+tail -2 /etc/hosts
+
+rm -rf /tmp/homebox
