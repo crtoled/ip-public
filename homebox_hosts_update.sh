83191fc387d5fca31173fe9ae196b76661ddb713
SA <no@email.com>
2023-09-14 11:57:49 +0900

    Fixed internal IP issue. Now, 192.168 and 10. will be excluded for updating /etd/hosts.

--- homebox_hosts_update.sh
+++ homebox_hosts_update.sh
@@ -9,8 +9,14 @@ awk '!/homebox/ && !/120s/' /etc/hosts > /tmp/tmp_hosts && mv /tmp/tmp_hosts /et
 
 git clone https://github.com/crtoled/ip-public.git /tmp/homebox
 #echo `cat /tmp/homebox/ip.txt | tail -n1` 'homebox'  >> /etc/hosts
-echo `cat /tmp/homebox/ip-box2022.txt | tail -n1` '     homebox'  >> /etc/hosts
-echo `cat /tmp/homebox/ip-120s.txt | tail -n1` '     120s'  >> /etc/hosts
+IPADDRESSES=(`cat /tmp/homebox/ip-box2022.txt | tail -n1`)
+IP=`echo ${IPADDRESSES[0]} '   homebox' | grep -v '^192.168\|^10'`  # Exclude the internal IPs.
+echo $IP >> /etc/hosts
+IP=`echo ${IPADDRESSES[1]} 'homebox' | grep -v '^192.168\|^10'`  # Exclude the internal IPs.
+echo $IP >> /etc/hosts
+
+#echo `cat /tmp/homebox/ip-box2022.txt | tail -n1` '     homebox'  >> /etc/hosts
+#echo `cat /tmp/homebox/ip-120s.txt | tail -n1` '     120s'  >> /etc/hosts
 
 tail -2 /etc/hosts
 
